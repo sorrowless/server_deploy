fc7de50b38dc6313c60d22aefaf2681f239a9774
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-01-28 17:38:35 +0300

    Add alpha version of server deploy code
    It is first working version of code which can deploy
    a vps.

---
+++ vps/manifests/base.pp
@@ -0,0 +1,81 @@
+include vps::users
+
+if $::operatingsystem == 'CentOS' {
+  package { 'epel-release':
+    ensure => present,
+  }
+}
+
+$packages = [ 'vim-common', 'rsync', 'mc', 'git' ]
+package { $packages:
+  ensure => present,
+}
+
+file { '/etc/sysconfig/iptables':
+  ensure => present,
+  content => template('vps/iptables.erb'),
+  notify => Service['iptables'],
+}
+
+service { 'iptables':
+  ensure => running,
+  enable => true,
+} 
+
+$sshd_port = hiera('sshd_port')
+$admin_user = hiera('admin_user')
+file { '/etc/ssh/sshd_config':
+  ensure => present,
+  content => template('vps/sshd_config.erb'),
+  notify => Service['sshd'],
+}
+
+service { 'sshd':
+  ensure => running,
+  enable => true,
+}
+
+$authorized_keys = hiera('authorized_keys')
+file { "/home/${admin_user}/.ssh":
+  ensure => directory,
+  owner => $admin_user,
+  group => $admin_user,
+} ->
+
+file { "/home/${admin_user}/.ssh/authorized_keys":
+  ensure => present,
+  owner => $admin_user,
+  group => $admin_user,
+  mode => '0600',
+  content => template('vps/authorized_keys.erb'),
+}
+
+# install and configure postfix
+$helo_regexp = hiera('postfix_helo_regexp', {})
+class { 'vps::mail::postfix':
+  myorigin => hiera('postfix_myorigin'),
+  helo_regexp => $helo_regexp, 
+  recipient_regexp => hiera('postfix_recipient_access'),
+  virtual_reroute => hiera('postfix_virtual_rewrite'),
+  mailcert => hiera('postfix_pemcert'),
+  mailkey => hiera('postfix_pemkey'),
+}
+
+
+# install and configure cyrus-imapd
+class { 'vps::mail::cyrus-imapd':
+  cyrus_admin => hiera('cyrus_admin'),
+  loginrealms => hiera('cyrus_loginrealms'),
+}
+
+
+# install and configure nginx and all nginx sites
+class { 'vps::nginx':
+  domain => hiera('nginx_domain'),
+  ssl_data => hiera('nginx_ssl'),
+}
+
+
+# install and configure mysql
+class { 'vps::mysql':
+}
---
+++ vps/manifests/nginx.pp
@@ -0,0 +1,113 @@
+class vps::nginx (
+  $domain,
+  $ssl_data = undef,
+  $ensure_package = 'present',
+  $ensure_service = 'running',
+){
+  
+  # we need php and php-xml for mail system, php-mbstring and php-mysql for rss
+  package { ['nginx', 'php-fpm', 'php', 'php-xml', 'php-mbstring', 'php-mysql', 'php-process']:
+    ensure => $ensure_package,
+  } ->
+
+  file { '/etc/nginx/ssl':
+    ensure => directory,
+  }
+
+  Package <| title == 'nginx' |> -> File<||> -> Service['nginx','php-fpm']
+
+  file { '/etc/nginx/nginx.conf':
+    ensure => present,
+    content => template('vps/http/nginx.conf'),
+  }
+
+  file { "/etc/nginx/conf.d/blog.${domain}.conf":
+    ensure => present,
+    content => template('vps/http/blog.conf.erb'),
+  }
+
+
+  file { "/etc/nginx/conf.d/dav.${domain}.conf":
+    ensure => present,
+    content => template('vps/http/dav.conf.erb'),
+  }
+  
+  file { "/etc/nginx/ssl/dav.${domain}.crt":
+    ensure => present,
+    content => $ssl_data['dav_cert'],
+  }
+  
+  file { "/etc/nginx/ssl/dav.${domain}.key":
+    ensure => present,
+    content => $ssl_data['dav_key'],
+  }
+
+
+  file { "/etc/nginx/conf.d/mail.${domain}.conf":
+    ensure => present,
+    content => template('vps/http/mail.conf.erb'),
+  }
+
+
+  file { "/etc/nginx/conf.d/rss.${domain}.conf":
+    ensure => present,
+    content => template('vps/http/rss.conf.erb'),
+  }
+
+  file { "/etc/nginx/ssl/rss.${domain}.crt":
+    ensure => present,
+    content => $ssl_data['rss_cert'],
+  }
+  
+  file { "/etc/nginx/ssl/rss.${domain}.key":
+    ensure => present,
+    content => $ssl_data['rss_key'],
+  }
+  
+  file { "/etc/init.d/ttrss-daemon":
+    ensure => present,
+    content => template('vps/http/ttrss-daemon'),
+    mode => '0755',
+    owner => 'root',
+    group => 'root',
+  } ->
+
+  service { 'ttrss-daemon':
+    ensure => running,
+    enable => true,
+  }
+
+
+  file { "/etc/nginx/conf.d/www.${domain}.conf":
+    ensure => present,
+    content => template('vps/http/www.conf.erb'),
+  }
+
+  file { "/etc/nginx/ssl/www.${domain}.crt":
+    ensure => present,
+    content => $ssl_data['www_cert'],
+  }
+  
+  file { "/etc/nginx/ssl/www.${domain}.key":
+    ensure => present,
+    content => $ssl_data['www_key'],
+  }
+
+  file { "/etc/php-fpm.d/www.conf":
+    ensure => present,
+    content => template('vps/http/php-fpm.conf'),
+    require => Package['php-fpm'],
+  }
+
+  exec { 'create dhparam':
+    path => '/bin:/usr/bin:/usr/sbin',
+    command => 'openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048',
+    creates => '/etc/nginx/ssl/dhparam.pem',
+  }
+
+  service { ['nginx', 'php-fpm']:
+    ensure => $ensure_service,
+    enable => true,
+  }
+  
+}
